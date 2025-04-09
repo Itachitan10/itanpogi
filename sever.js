fde6d56798ac2d0edaa0574907819c0240877c01
unknown <youremail@example.com>
2025-04-09 23:14:31 +0800

    o dont  know

--- sever.js
+++ sever.js
@@ -1,36 +1,35 @@
-const express = require('express');  
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
-const ejs = require('ejs');
-const path = require('path');
 
+const ejs = require("ejs");
+const path = require("path");
 
-
-app.use(bodyParser.urlencoded({ extended: true }));  
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.engine("html", ejs.renderFile);
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "frontend/html"));
+app.use("/css", express.static(path.join(__dirname, "frontend/css")));
+app.use("/html", express.static(path.join(__dirname, "frontend/html")));
 
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'html');  
-app.set('views', path.join(__dirname, '../frontend/html')); 
-app.use('/css', express.static(path.join(__dirname, '../frontend/css')));
-app.use('/html', express.static(path.join(__dirname, '../frontend/html')));
+// import routes
+const front = require("./backend/routes/register");
+const login = require("./backend/routes/login");
 
-
-// import routes 
-const front = require('./routes/register');
-const login = require('./routes/login')
-
-const cart = require('./routes/cart')
+const cart = require("./backend/routes/cart");
 
 // all routes
-app.use('/', front);
-app.use('/', login);
-app.use('/', cart);
+app.use("/", front);
+app.use("/", login);
+app.use("/", cart);
 
 // server
+const port = 3000;
+
+
 app.listen(port, (err) => {
-    if (err) throw err;
-    console.log('Server is running on port 3000');
+  if (err) throw err;
+  console.log("Server is running on port 3000");
 });
