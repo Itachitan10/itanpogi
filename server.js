b47052a3c8a3874c98511b472507bb9a4e233faa
unknown <youremail@example.com>
2025-04-13 10:38:06 +0800

    modifide server js and git ignore

--- server.js
+++ server.js
@@ -43,10 +43,10 @@ app.use("/", cart);
 app.use('/', dash)
 
 // server
-const port = 3000;
 
 
-port = process.env.PORT || 3000;
+
+ const port = process.env.PORT || 3000;
 app.listen(port, (err) => {
   if (err) throw err;
   console.log("Server is running on port 3000");
