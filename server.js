12a3410ebb3a95ef009aa85573c43b391bc17b09
unknown <youremail@example.com>
2025-04-14 19:20:44 +0800

    afaafsdaf

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use("/", cart);
 app.use('/', dash)
 
 // server
-app.get("/register", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/frontend/html/register.html"));
 });
 
